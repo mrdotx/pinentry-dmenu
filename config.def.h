69d369f6a2f6d66986ddf124198e345baae18aba
mrdotx <klassiker@gmx.de>
2021-07-31 10:53:44 +0200

    indentation changed from tabs to spaces

--- config.def.h
+++ config.def.h
@@ -8,24 +8,24 @@ static unsigned int lineheight = 22;
 static unsigned int borderwidth = 2;
 static unsigned int minpwlen = 32;
 static unsigned int alphas[SchemeLast][2] = {
-	/* values between 0 (transparent) and 255 (opaque) */
-	/*          foreground, background */
-	[SchemePrompt] = { 255, 255 },
-	[SchemeNormal] = { 255, 230 },
-	[SchemeSelect] = { 255, 255 },
-	[SchemeDesc]   = { 255, 230 },
+    /* values between 0 (transparent) and 255 (opaque) */
+    /*          foreground, background */
+    [SchemePrompt] = { 255, 255 },
+    [SchemeNormal] = { 255, 230 },
+    [SchemeSelect] = { 255, 255 },
+    [SchemeDesc]   = { 255, 230 },
 };
 
 static const char *position = "center";
 static const char *fonts[] = {
-	"DejaVuSansMono Nerd Font:pixelsize=16:antialias=true:autohint=true"
+    "DejaVuSansMono Nerd Font:pixelsize=16:antialias=true:autohint=true"
 };
 static const char *prompt = NULL;
 static const char *asterisk = "*";
 static const char *colors[SchemeLast][2] = {
-	/*                foreground, background */
-	[SchemePrompt] = { "#cccccc", "#4185d7" },
-	[SchemeNormal] = { "#cccccc", "#000000" },
-	[SchemeSelect] = { "#ffffff", "#4185d7" },
-	[SchemeDesc]   = { "#cccccc", "#000000" },
+    /*                foreground, background */
+    [SchemePrompt] = { "#cccccc", "#4185d7" },
+    [SchemeNormal] = { "#cccccc", "#000000" },
+    [SchemeSelect] = { "#ffffff", "#4185d7" },
+    [SchemeDesc]   = { "#cccccc", "#000000" },
 };
